ba4c4d621eb8f2fcf43beca428732d9f3c8df89c
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2024-02-21 18:24:33 -0800

    Time: 39 ms (45.39%) | Memory: 16.5 MB (98.45%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,29 +1,18 @@
-dictionary = defaultdict(int)
 class Solution:
-    
-    def is_valid(self,s1):
-        if s1 == "":
-            return True
-        return not (s1.startswith("0") or int(s1) == 0 or int(s1)>26)
-    
-    def calculate(self,s,s1,start):
-        global dictionary
-        print(s1,s[start:])
-        if not self.is_valid(s1):
-            return 0
-        if start>=len(s):
-            return 1
-        if s[start:] in dictionary:
-            return dictionary[s[start:]]
-        
-        ans = 0
-        count= 0
-        for i in range(start,len(s)) :
-            count += self.calculate(s,s[start:i+1],i+1)
-        ans += count
-        dictionary[s[start:]] = ans
-        return ans
-        
-
     def numDecodings(self, s: str) -> int:
-        return self.calculate(s[:],"",0)
+        dp = [0]*len(s)
+        if s[0]!='0':
+            dp[0] = 1
+        for i in range(1,len(s)):
+            count = 0
+            if s[i]!='0':
+                count = dp[i-1]
+            if s[i-1]!='0' and int(s[i-1:i+1])<=26:
+                if i>1:
+                    dp[i] = count+dp[i-2]
+                else:
+                    dp[i] = count+1
+            else:
+                dp[i] = count
+        return dp[-1]
+            
