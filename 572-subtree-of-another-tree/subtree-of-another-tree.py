c6aa4d001e55c92ed67fa15f2405a95b232d2ef3
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2024-07-19 18:57:50 -0400

    Time: 70 ms (79.02%) | Memory: 16.8 MB (34.54%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -5,21 +5,54 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def checkSubTree(self,root,subRoot):
-        if (root and not subRoot) or (not root and subRoot):
-            return False
-        if not root and not subRoot:
-            return True
-        return root.val == subRoot.val and self.checkSubTree(root.left,subRoot.left) and self.checkSubTree(root.right,subRoot.right)
-
-    def isSubTreeUtil(self,root,subRoot):
+    def serializeTree(self,root,treeList):
         if not root:
-            return False
-        if root.val == subRoot.val:
-            if self.checkSubTree(root,subRoot):
-                return True
-        return self.isSubTreeUtil(root.left,subRoot) or self.isSubTreeUtil(root.right,subRoot)
+            treeList.append("#")
+            return
+        treeList.append("^"+str(root.val))
+        self.serializeTree(root.left,treeList)
+        self.serializeTree(root.right,treeList)
+
+    def kmp(self,string,substring):
+        n = len(substring)
+        lps = [-1]*n
+        i,j = 0,1
+        
+        while j<n:
+            if substring[i] == substring[j]:
+                lps[j] = i
+                i+=1
+                j+=1
+            else:
+                if lps[i-1] > 0:
+                    i = lps[i-1]+1
+                else:
+                    i = 0
+                    lps[j] = -1
+                    j+=1
+        i,j = 0,0
+        while i<len(string):
+            if string[i] == substring[j]:
+                i+=1
+                j+=1
+                if j == n:
+                    return True
+            else:
+                if j == 0:
+                    i+=1
+                else:
+                    j = lps[j-1]+1
+        return False
+        
+                
 
 
+    
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        return self.isSubTreeUtil(root,subRoot)
+        treeList,subTreeList = [],[]
+        self.serializeTree(root,treeList)
+        self.serializeTree(subRoot,subTreeList)
+        treeStr = "".join(treeList)
+        subTreeStr = "".join(subTreeList)
+        print(treeStr,subTreeStr)
+        return self.kmp(treeStr,subTreeStr)
