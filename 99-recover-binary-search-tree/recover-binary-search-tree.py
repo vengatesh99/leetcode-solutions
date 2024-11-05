6f7dd65b993f9cb75bd6a8cf0592f8f5dc128af7
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2024-11-05 10:03:40 -0500

    Time: 0 ms (100.00%) | Memory: 17 MB (13.94%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/recover-binary-search-tree.py
@@ -5,45 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.inorder = []
-    def recoverUtil(self,root,inorder):
-        if not root:
-            return
-        self.recoverUtil(root.left,inorder)
-        inorder.append(root.val)
-        self.recoverUtil(root.right,inorder)
-
-    def traverse(self,root,swap1,swap2):
-        if not root:
-            return
-        self.traverse(root.left,swap1,swap2)
-        if root.val == swap1:
-            root.val = swap2
-        elif root.val == swap2:
-            root.val = swap1
-            return
-        self.traverse(root.right,swap1,swap2)
     def recoverTree(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        sol = Solution()
-        inorder = sol.inorder
-        self.recoverUtil(root,inorder)
-        print(inorder)
-        swap = 0
-        swap1,swap2 = -1,-1
-        for i in range(1,len(inorder)):
-            if inorder[i-1]>inorder[i]:
-                if swap>0:
-                    swap2 = i
-                    break
-                swap1 = i-1
-                swap+=1
-        if swap2 == -1:
-            swap2 = swap1+1
-        self.traverse(root,inorder[swap1],inorder[swap2])
-        
-            
+        def swapNodes(root):
+            nonlocal swap1,swap2,pred
+            if not root:
+                return
+            swapNodes(root.left)
+            if pred:
+                if pred.val>root.val:
+                    swap2 = root
+                    if not swap1:
+                        swap1 = pred
+                    else:
+                        return
+            pred = root
+            swapNodes(root.right)
+    
+        swap1 = swap2 = pred = None
+        swapNodes(root)
+        swap1.val,swap2.val = swap2.val,swap1.val
+
         
