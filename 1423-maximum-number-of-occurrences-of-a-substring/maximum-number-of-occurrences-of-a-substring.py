0951b6529d9879bd4bd821776578259a5eda5520
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2024-11-09 10:53:19 -0500

    Time: 147 ms (33.07%) | Memory: 18.4 MB (37.29%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.py
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.py
@@ -1,11 +1,28 @@
 class Solution:
     def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
-        counts = dict()
-        for j in range(len(s)-minSize+1):
-            word = s[j:j+minSize]
-            if word in counts:
-                counts[word]+=1
-            else:
-                if len(collections.Counter(word))<=maxLetters:
-                    counts[word]=1
-        return max(counts.values()) if len(counts)!=0 else 0
+        substring_dict = defaultdict(int)
+        i,j = 0,0
+        n = len(s)
+        ans = 0
+        currentDict = defaultdict(int)
+        while j<n:
+            currentDict[s[j]]+=1
+            while i<=j and len(currentDict)>maxLetters:
+                currentDict[s[i]]-=1
+                if currentDict[s[i]] == 0:
+                    del currentDict[s[i]]
+                i+=1
+            while i<=j and j-i+1>maxSize:
+                currentDict[s[i]]-=1
+                if currentDict[s[i]] == 0:
+                    del currentDict[s[i]]
+                i+=1
+            while i<=j and j-i+1>=minSize:
+                substring_dict[s[i:j+1]]+=1
+                ans = max(ans,substring_dict[s[i:j+1]])
+                currentDict[s[i]]-=1
+                if currentDict[s[i]] == 0:
+                    del currentDict[s[i]]
+                i+=1
+            j+=1
+        return ans
