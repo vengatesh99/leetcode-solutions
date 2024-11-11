5bd804635218840b5796419a2ea1fcacbff42d6f
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2024-11-10 23:57:58 -0500

    Time: 226 ms (5.07%) | Memory: 16.7 MB (47.53%) - LeetSync

---
+++ 1051-shortest-way-to-form-string/shortest-way-to-form-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shortestWay(self, source: str, target: str) -> int:
+
+        # To check if to_check is subsequence of in_string
+        def is_subsequence(to_check, in_string):
+            i = j = 0
+            while i < len(to_check) and j < len(in_string):
+                if to_check[i] == in_string[j]:
+                    i += 1
+                j += 1
+
+            return i == len(to_check)
+
+        # Set of all characters of the source. We could use a boolean array as well.
+        source_chars = set(source)
+
+        # Check if all characters of the target are present in the source
+        # If any character is not present, return -1
+        for char in target:
+            if char not in source_chars:
+                return -1
+
+        # Concatenate source until the target is a subsequence
+        # of the concatenated string
+        concatenated_source = source
+        count = 1
+        while not is_subsequence(target, concatenated_source):
+            concatenated_source += source
+            count += 1
+
+        # Number of concatenations done
+        return count
