7c0dedb3d2a733da900380744c8ffc8653674a18
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2024-11-11 19:38:23 -0500

    Time: 15 ms (35.13%) | Memory: 16.7 MB (23.38%) - LeetSync

--- 1051-shortest-way-to-form-string/shortest-way-to-form-string.py
+++ 1051-shortest-way-to-form-string/shortest-way-to-form-string.py
@@ -1,32 +1,25 @@
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
-
-        # To check if to_check is subsequence of in_string
-        def is_subsequence(to_check, in_string):
-            i = j = 0
-            while i < len(to_check) and j < len(in_string):
-                if to_check[i] == in_string[j]:
-                    i += 1
-                j += 1
-
-            return i == len(to_check)
-
-        # Set of all characters of the source. We could use a boolean array as well.
-        source_chars = set(source)
-
-        # Check if all characters of the target are present in the source
-        # If any character is not present, return -1
-        for char in target:
-            if char not in source_chars:
+        ans = 0
+        source_set = set(source)
+        target_set = set(target)
+        # print(source_set)
+        # print(target_set)
+        for ch in target:
+            if ch not in source_set:
                 return -1
-
-        # Concatenate source until the target is a subsequence
-        # of the concatenated string
-        concatenated_source = source
-        count = 1
-        while not is_subsequence(target, concatenated_source):
-            concatenated_source += source
-            count += 1
-
-        # Number of concatenations done
-        return count
+        i,j = 0,0
+        n,m = len(target),len(source)
+        ans = 1
+        while j<n:
+            if i == len(source):
+                i = 0
+                ans+=1
+                continue
+            if source[i]!=target[j]:
+                i+=1
+            elif source[i] == target[j]:
+                i+=1
+                j+=1
+        return ans
+        
