58b000e14c817487bce9cd0e549c5b233ede8aa8
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2024-12-22 05:55:00 -0500

    Time: 623 ms (8.08%) | Memory: 18 MB (26.62%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        def dp(curr,n,clipboard):
+            if curr == n:
+                return 0
+            if curr > n:
+                return 1000
+
+            opt1 = 2+dp(curr*2,n,curr)
+            opt2 = 1+dp(curr+clipboard,n,clipboard)
+            return min(opt1,opt2)
+        if n == 1:
+            return 0
+        return 1+dp(1,n,1)
