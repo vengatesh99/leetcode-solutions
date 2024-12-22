803f3d6d0da1d1edd5fdf295ff14fe3b93b4ae5c
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2024-12-22 05:57:30 -0500

    Time: 106 ms (49.15%) | Memory: 28.3 MB (15.06%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -1,14 +1,17 @@
 class Solution:
     def minSteps(self, n: int) -> int:
+        memo = {}
         def dp(curr,n,clipboard):
             if curr == n:
                 return 0
             if curr > n:
                 return 1000
-
+            if (curr,clipboard) in memo:
+                return memo[(curr,clipboard)]
             opt1 = 2+dp(curr*2,n,curr)
             opt2 = 1+dp(curr+clipboard,n,clipboard)
-            return min(opt1,opt2)
+            memo[(curr,clipboard)] = min(opt1,opt2)
+            return memo[(curr,clipboard)]
         if n == 1:
             return 0
         return 1+dp(1,n,1)
