c2bfafcb2797e087dbfa5a612c6de11339614b3c
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-01-13 20:58:25 -0500

    Time: 7 ms (75.00%) | Memory: 17.6 MB (45.05%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -1,13 +1,22 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        hashmap = {A[i]:i for i in range(len(A))}
+        # hashmap = {A[i]:i for i in range(len(A))}
+        hashset = set()
         C = [0]*len(A)
         for i in range(len(A)):
-            j = i
+            # j = i
+            # ct = 0
+            # while j>=0:
+            #     if i>=hashmap[B[j]]:
+            #         ct+=1
+            #     j-=1
             ct = 0
-            while j>=0:
-                if i>=hashmap[B[j]]:
-                    ct+=1
-                j-=1
-            C[i] = ct
+            if B[i] in hashset:
+                ct+=1
+            hashset.add(B[i])
+            if A[i] in hashset:
+                ct+=1
+            hashset.add(A[i])
+            C[i] = C[i-1] + ct
+            # C[i] = ct
         return C
