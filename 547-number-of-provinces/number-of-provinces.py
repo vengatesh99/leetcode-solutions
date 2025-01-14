54b5389637cd6ddd77d2de817f45ba72caebf547
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-01-14 16:45:02 -0500

    Time: 15 ms (18.16%) | Memory: 18.8 MB (50.29%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,22 +1,31 @@
+class UnionFind:
+    def __init__(self,sz):
+        self.parent = [i for i in range(sz)]
+        self.rank = [1]*sz
+    def find(self,node):
+        while node != self.parent[node]:
+            self.parent[node] = self.parent[self.parent[node]]
+            node = self.parent[node]
+        return node
+    def union(self,n1,n2):
+        p1 = self.find(n1)
+        p2 = self.find(n2)
+        if p1 == p2:
+            return 0
+        if self.rank[p1]>self.rank[p2]:
+            self.parent[p2] = p1
+            self.rank[p1]+=1
+        else:
+            self.parent[p1] = p2
+            self.rank[p2]+=1
+        return 1
+        
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        graph = defaultdict(set)
-        n = len(isConnected)
-        visit = set()
-        for i in range(n):
-            for j in range(n):
-                if i!=j and isConnected[i][j]:
-                    graph[i].add(j)
-                    graph[j].add(i)
-        def dfs(node):
-            visit.add(node)
-            for neib in graph[node]:
-                if neib not in visit:
-                    dfs(neib)
-        provinces = 0
-        print(graph)
-        for i in range(n):
-            if i not in visit:
-                dfs(i)
-                provinces+=1
-        return provinces
+        uf = UnionFind(len(isConnected))
+        ans = len(isConnected)
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected)):
+                if i != j and isConnected[i][j]:
+                    ans -= uf.union(i,j)
+        return ans
