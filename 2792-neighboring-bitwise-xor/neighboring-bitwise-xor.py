224e42908701beeea5e9cf25bbf74e7d5af0f950
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-01-17 00:03:07 -0500

    Time: 37 ms (80.29%) | Memory: 22.3 MB (48.90%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
@@ -0,0 +1,8 @@
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        first = 0
+        last = 0
+        for n in derived:
+            if n:
+                last = ~last
+        return first == last
