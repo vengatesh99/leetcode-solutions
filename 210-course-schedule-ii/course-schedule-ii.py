e3553ed47bbbbd381f866309b0187a5c48091737
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-01-18 17:35:47 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (26.39%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,31 +1,26 @@
 class Solution:
-    WHITE = 1
-    GRAY = 2
-    BLACK = 3
-
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        topo_sort = []
-        is_possible = True
-        color = {k:Solution.WHITE for k in range(numCourses)}
-        
-        def dfs(start):
-            nonlocal is_possible
-
-            if not is_possible:
-                return
-            color[start] = Solution.GRAY
-            for neib in adj_list[start]:
-                if color[neib] == Solution.WHITE:
-                    dfs(neib)
-                elif color[neib] == Solution.GRAY:
-                    is_possible = False
-            color[start] = Solution.BLACK
-            topo_sort.append(start)
-        
         adj_list = defaultdict(list)
-        for arr in prerequisites:
-            adj_list[arr[1]].append(arr[0])
+        for course,pre in prerequisites:
+            adj_list[pre].append(course)
+        visit = set()
+        ans = []
+        color = {i:0 for i in range(numCourses)}
+        def dfs(node):
+            nonlocal ans
+            if color[node] == 1:
+                return False
+            if color[node] == 2:
+                return True
+            color[node] = 1
+            for neib in adj_list[node]:
+                if not dfs(neib):
+                    return False
+            color[node] = 2
+            ans.insert(0,node)
+            return True
         for i in range(numCourses):
-            if color[i] == Solution.WHITE:
-                dfs(i)
-        return topo_sort[::-1] if is_possible else []
+            if color[i] == 0:
+                if not dfs(i):
+                    return []
+        return ans
