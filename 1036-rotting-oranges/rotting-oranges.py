8cc62554f69299ee96c647c2e80afc3dc404ab5d
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-01-18 04:41:50 -0500

    Time: 5 ms (38.26%) | Memory: 17.9 MB (15.04%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,33 +1,27 @@
 class Solution:
-    def isSafe(self,X,Y,n,m):
-        return X>=0 and X<n and Y>=0 and Y<m
-
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        n,m = len(grid),len(grid[0])
-        minutes = -1
-        fresh = 0
-        queue = deque()
-        directions = [(0,-1),(0,1),(1,0),(-1,0)]
-        visited = set()
-        for i in range(n):
-            for j in range(m):
+        q = deque()
+        seen = set()
+        m,n = len(grid),len(grid[0])
+        minutes = 0
+        ans = 0
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == 2:
-                    queue.append((i,j))
-                elif grid[i][j] == 1:
-                    fresh+=1
-        queue.append((-1,-1))
-        while queue:
-            row,col = queue.popleft()
-            if row == -1:
-                minutes+=1
-                if queue:
-                    queue.append((-1,-1))
-            else:
-                for x,y in directions:
-                    if self.isSafe(row+x,col+y,n,m) and grid[row+x][col+y] == 1:
-                        # visited.add((row+x,col+y))
-                        queue.append((row+x,col+y))
-                        grid[row+x][col+y] = 2
-                        fresh-=1
-        
-        return minutes if fresh == 0 else -1
+                    q.append((i,j,0))
+                    seen.add((i,j))
+        while q:
+            r,c,minutes = q.popleft()
+            ans = minutes
+            for dx,dy in directions:
+                nr,nc = r+dx,c+dy
+                if min(nr,nc)>=0 and nr<m and nc<n and (nr,nc) not in seen and grid[nr][nc] == 1:
+                    seen.add((nr,nc))
+                    q.append((nr,nc,minutes+1))
+            
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] and (i,j) not in seen:
+                    return -1
+        return ans
