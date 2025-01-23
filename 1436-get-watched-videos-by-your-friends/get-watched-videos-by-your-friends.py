2a36755f584782d1c6e74233e17a93d93421a680
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-01-23 00:24:29 -0800

    Time: 23 ms (54.05%) | Memory: 19.7 MB (17.15%) - LeetSync

---
+++ 1436-get-watched-videos-by-your-friends/get-watched-videos-by-your-friends.py
@@ -0,0 +1,29 @@
+class Solution:
+    def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], id: int, level: int) -> List[str]:
+        adj_list = {}
+        n = len(watchedVideos)
+        for i,friend in enumerate(friends):
+            adj_list[i] = friend
+        q = deque()
+        q.append(id)
+        visit = set()
+        visit.add(id)
+        hashmap = defaultdict(int)
+        while q and level:
+            sz = len(q)
+            while sz:
+                node = q.popleft()
+                for neib in adj_list[node]:
+                    if neib not in visit:
+                        q.append(neib)
+                        visit.add(neib)
+                sz-=1
+            level-=1
+        while q:
+            node = q.popleft()
+            for vid in watchedVideos[node]:
+                hashmap[vid]+=1
+            if node in visit:
+                continue
+            visit.add(node)
+        return sorted(hashmap.keys(), key = lambda x: (hashmap[x],x))
