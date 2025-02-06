2f1d1d66c13f90aca29ced143228393e59b2283a
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-06 11:07:24 -0500

    Time: 7 ms (23.76%) | Memory: 18.1 MB (12.02%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,33 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        hash_map = {}
-        wordDictSet = set()
-        for word in wordDict:
-            wordDictSet.add(word)
-
-        def dp(string):
-            if string == "":
-                return False
-            if len(string) == 1 and string not in wordDictSet:
-                return False
-            if string in wordDictSet:
+        dictionary = set(wordDict)
+        memo = {}
+        def dfs(ind):
+            if ind == len(s):
                 return True
-            if string in hash_map:
-                return hash_map[string]
-            
-            left,right = "",""
-            for i in range(len(string)):
-                left += string[i]
-                right = string[i+1:]
-                hash_map[left] = dp(left)
-                if hash_map[left]:
-                    hash_map[right] = dp(right)
-                hash_map[string] = hash_map[left] and hash_map[right]
-                if hash_map[string]:
-                    return hash_map[string]
+            if ind in memo:
+                return False
+            for i in range(ind,len(s)):
+                if s[ind:i+1] in wordDict and dfs(i+1):
+                    return True
+            memo[ind] = False
             return False
-                
-        return dp(s)
-        # return hash_map[s]
-
-
+        return dfs(0)
