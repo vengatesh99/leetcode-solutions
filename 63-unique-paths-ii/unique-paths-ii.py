842e2a2b369e5678d1277e93e64a368a8d5038c4
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-07 13:19:53 -0500

    Time: 1 ms (37.69%) | Memory: 18.4 MB (5.18%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,19 +1,28 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        hash_map = {}
-        def dp(row,col):
-            if row<0 or col<0:
-                return 0
-            if row == 0 and col ==0:
-                return 1 if obstacleGrid[row][col]==0 else 0
-            if obstacleGrid[row][col] == 1:
-                return 0
-            if (row,col) in hash_map:
-                return hash_map[(row,col)]
-            path = dp(row-1,col)+dp(row,col-1)
-            hash_map[(row,col)] = path
-            return path
-        n,m = len(obstacleGrid),len(obstacleGrid[0])
-        if n==1 and m == 1:
-            return 1 if obstacleGrid[n-1][m-1]==0 else 0
-        return dp(n-1,m-1)
+        m,n = len(obstacleGrid),len(obstacleGrid[0])
+        # paths = 0
+        visit = set()
+        if obstacleGrid[0][0] or obstacleGrid[m-1][n-1]:
+            return 0
+        directions = [[0,1],[1,0]]
+        memo = {}
+        def dfs(row,col):
+            # nonlocal paths
+            visit.add((row,col))
+            if row == m-1 and col == n-1:
+                return 1
+            if (row,col) in memo:
+                return memo[(row,col)]
+            paths = 0
+            for r,c in directions:
+                newR,newC = row+r,col+c
+                if  min(newR,newC)<0 or newR==m or newC == n or (newR,newC) in visit or obstacleGrid[newR][newC]:
+                    continue
+                paths+=dfs(newR,newC)
+                visit.remove((newR,newC))
+            memo[(row,col)] = paths
+            return memo[(row,col)]
+        return dfs(0,0)
+
+            
