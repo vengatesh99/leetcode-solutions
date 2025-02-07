40e7cd114821af4a43b0aefffd8243e04adec526
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-07 01:52:19 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (5.04%) - LeetSync

--- 545-boundary-of-binary-tree/boundary-of-binary-tree.py
+++ 545-boundary-of-binary-tree/boundary-of-binary-tree.py
@@ -5,53 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    leftB = []
-    rightB = []
-    leaves = []
-    def addLeftBoundary(self,node):
-        if not node:
-            return
-        if not node.left and not node.right:
-            return
-        self.leftB.append(node.val)
-        if node.left:
-            self.addLeftBoundary(node.left)
-        else:
-            self.addLeftBoundary(node.right)
-        
-    def addRightBoundary(self,node):
-        
-        if not node:
-            return
-        if not node.left and not node.right:
-            return
-        print(node.val)
-        if node.right:
-            self.addRightBoundary(node.right)
-        else:
-            self.addRightBoundary(node.left)
-        self.rightB.append(node.val)
-        
-
-    def addLeaves(self,node):
-        if node is None:
-            return
-        if node.left is None and node.right is None:
-            self.leaves.append(node.val)
-            return
-        self.addLeaves(node.left)
-        self.addLeaves(node.right)
-
     def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
-        self.leftB = []
-        self.rightB = []
-        self.leaves = []
         if root.left == None and root.right == None:
             return [root.val]
-        self.addLeftBoundary(root.left)
-        self.addRightBoundary(root.right)
-        self.addLeaves(root)
-        print(self.leaves)
-        ans = [root.val]+self.leftB+self.leaves+self.rightB
-
-        return ans
+        leftB = root.left
+        rightB = root.right
+        leftList = []
+        rightList = []
+        leavesList = []
+        while leftB and (leftB.left or leftB.right):
+            leftList.append(leftB.val)
+            leftB = leftB.left if leftB.left else leftB.right
+        while rightB and (rightB.left or rightB.right):
+            rightList.append(rightB.val)
+            rightB = rightB.right if rightB.right else rightB.left
+        def getLeaves(tempRoot):
+            if not tempRoot:
+                return
+            if not tempRoot.left and not tempRoot.right:
+                leavesList.append(tempRoot.val)
+                return
+            getLeaves(tempRoot.left)
+            getLeaves(tempRoot.right)
+        getLeaves(root)
+        return [root.val]+leftList + leavesList + rightList[::-1]
