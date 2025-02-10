a2d9766d9eb98e266129da420f43fdf02a0f0640
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-10 12:35:44 -0500

    Time: 31 ms (43.79%) | Memory: 23.7 MB (17.84%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.py
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.py
@@ -1,18 +1,17 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
+        target = sum(stones)//2
         n = len(stones)
-        sum_weights = sum(stones)
-        half_weight = sum_weights // 2
         memo = {}
-        def dp(idx,target):
-            if idx == n or target == 0:
+        def findSum(i,target):
+            if i == n:
                 return 0
-            if (idx,target) in memo:
-                return memo[(idx,target)]
-            if target - stones[idx] < 0:
-                return dp(idx+1,target)
-            ans = max(stones[idx] + dp(idx+1,target-stones[idx]),dp(idx+1,target))
-            memo[(idx,target)] = ans
-            return memo[(idx,target)]
-        closest_half = dp(0,half_weight)
-        return sum_weights - 2*closest_half
+            if target-stones[i]<0:
+                return findSum(i+1,target)
+            if (i,target) in memo:
+                return memo[(i,target)]
+            maxsum = max(stones[i]+findSum(i+1,target-stones[i]),findSum(i+1,target))
+            memo[(i,target)] = maxsum
+            return maxsum
+
+        return sum(stones)-2*findSum(0,target)
