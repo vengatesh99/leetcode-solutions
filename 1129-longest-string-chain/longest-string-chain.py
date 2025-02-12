43c48036dfbbc645376131f22185c7c0138367a8
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-12 17:26:29 -0500

    Time: 848 ms (21.23%) | Memory: 18.4 MB (37.23%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.py
+++ 1129-longest-string-chain/longest-string-chain.py
@@ -1,29 +1,17 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        sorted_words = sorted(words, key = lambda x: len(x))
-        memo = {}
-        predecessor = defaultdict(set)
-        def isPredecessor(pred,actual):
-            length = 0
-            for i in range(len(actual)):
-                if pred == actual[:i]+actual[i+1:]:
-                    length+=1
+        dp = [1 for _ in words]
+        words.sort(key = lambda x: len(x))
+        def isChain(w1,w2):
+            c = 0
+            for k in range(len(w2)):
+                if w2[:k]+w2[k+1:] == w1:
+                    c+=1
                     break
-            return length !=0
-        def dp(index):
-            if index == 0:
-                return 1
-            if index in memo:
-                return memo[index]
-            ans = 1
-            for i in range(index):
-                if len(sorted_words[index]) - len(sorted_words[i]) == 1 and isPredecessor(sorted_words[i],sorted_words[index]):
-                    predecessor[sorted_words[index]].add(sorted_words[i])
-                    ans = max(ans,1+dp(i))
-            memo[index] = ans
-            return memo[index]
-        ans = 1
+            return c!=0
+            
         for i in range(1,len(words)):
-            ans = max(ans,dp(i))
-        return ans
-                
+            for j in range(i-1,-1,-1):
+                if len(words[i])-len(words[j]) == 1 and isChain(words[j],words[i]):
+                    dp[i] = max(dp[i],dp[j]+1)
+        return max(dp)
