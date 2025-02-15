ea0aa092728c1de136e360302ef98826ee7ea3f4
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-15 16:18:32 -0500

    Time: 50 ms (17.51%) | Memory: 18 MB (24.42%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -1,17 +1,32 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        memo = {}
-        def dp(t):
-            if t<0:
-                return 0
-            if t == 0:
-                return 1
-            if t in memo:
-                return memo[t]
-            ways = 0
+        # memo = {}
+        # def dp(t):
+        #     if t<0:
+        #         return 0
+        #     if t == 0:
+        #         return 1
+        #     if t in memo:
+        #         return memo[t]
+        #     ways = 0
+        #     for num in nums:
+        #         ways += dp(t-num)
+        #     memo[t] = ways
+        #     return ways
+        # return dp(target)
+
+        #Tabulation
+        # 1. Base case
+        # 2. initiliaze i,j
+        # 3. Convert recurrence to forloop statements
+        dp = [0 for _ in range(target+1)]
+        dp[0] = 1
+        M = len(dp)
+        for i in range(M):
             for num in nums:
-                ways += dp(t-num)
-            memo[t] = ways
-            return ways
-        return dp(target)
+                if num<=i:
+                    dp[i]+=dp[i-num]
+        return dp[target]
+                
+        
