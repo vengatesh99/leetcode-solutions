ef607a271b51b180d938887a143235a777a3a396
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-15 13:14:26 -0500

    Time: 376 ms (41.14%) | Memory: 61.3 MB (31.15%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,15 +1,35 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[0]*(amount+1)]*(len(coins)+1)
-        n = len(coins)
-        for i in range(n):
+        N = len(coins)
+        # memo = {}
+        # def dp(amt,idx):
+        #     if amt == 0:
+        #         return 1
+        #     if idx == n or amt<0:
+        #         return 0
+        #     if(amt,idx) in memo:
+        #         return memo[(amt,idx)]
+        #     # ways = 0
+        #     ways = dp(amt-coins[idx],idx) + dp(amt,idx+1)
+        #     memo[(amt,idx)] = ways
+        #     return ways
+        # w = dp(amount,0)
+        # return w
+
+        #Tabulation
+        #1. Base Case
+        #2. Identify States
+        #3. Copy the recursion
+
+        dp = [[0 for i in range(0,amount+1)] for _ in range(N+1)]
+        m,n = len(dp),len(dp[0])
+        for i in range(m):
             dp[i][0] = 1
-        for i in range(n):
-            for j in range(1,amount+1):
-                if coins[i]>j:
-                    dp[i][j] = dp[i-1][j]
+        for i in range(1,m):
+            for j in range(1,n):
+                if j>=coins[i-1]:
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j]
                 else:
-                    dp[i][j] = dp[i-1][j-coins[i]]+dp[i][j]
-        return dp[n][amount]
-
-
+                    dp[i][j] = dp[i-1][j]
+        return dp[N][amount]
+    
