ffec079d8b93920bb15d7934b2eb7079bb394129
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-17 10:10:32 -0500

    Time: 143 ms (50.11%) | Memory: 33 MB (76.40%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,23 +1,19 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         n,m = len(matrix),len(matrix[0])
-        matrix_copy = [[0 for _ in range(m)] for _ in range(n)]
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+        length = 0
         for i in range(n):
-            for j in range(m):
-                matrix[i][j] = int(matrix[i][j])
-        max_len = 0
-        for i in range(n):
-            for j in range(m):
-                if i == 0 or j == 0:
-                    matrix_copy[i][j] = matrix[i][j]
-                    max_len = max(max_len,matrix_copy[i][j])
-                    continue
-                elif matrix[i][j] == 0:
-                    continue
-                matrix_copy[i][j] = min(matrix_copy[i][j-1],matrix_copy[i-1][j-1],matrix_copy[i-1][j])+1
-                
-                max_len = max(max_len,matrix_copy[i][j])
-        print(matrix_copy)
-        return max_len**2
-
-        
+            dp[i][0] = int(matrix[i][0])
+            length = max(length,dp[i][0])
+        for j in range(m):
+            dp[0][j] = int(matrix[0][j])
+            length = max(length,dp[0][j])
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][j] == "0": 
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+                length = max(length,dp[i][j])
+        return length**2
