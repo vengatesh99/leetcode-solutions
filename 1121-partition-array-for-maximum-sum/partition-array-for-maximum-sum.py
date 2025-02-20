440717c7b76af868fc05bedd9f5d4cbe185129ed
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-20 17:05:17 -0500

    Time: 235 ms (91.97%) | Memory: 18 MB (43.70%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
@@ -1,19 +1,29 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n = len(arr)
-        memo = {}
-        def dp(ind):
-            if ind == n:
-                return 0
-            if ind in memo:
-                return memo[ind]
-            num = 0
+        # memo = {}
+        # def dp(ind):
+        #     if ind == n:
+        #         return 0
+        #     if ind in memo:
+        #         return memo[ind]
+        #     num = 0
+        #     s = 0
+        #     partition = 0
+        #     for i in range(ind,min(ind+k,n)):
+        #         num = max(num,arr[i])
+        #         s = max(s,(i-ind+1)*(num)+dp(i+1))
+        #     memo[ind] = s
+        #     return s
+        # ans = dp(0)
+        dp = [0 for _ in range(n+1)]
+        for i in range(n-1,-1,-1):
             s = 0
-            partition = 0
-            for i in range(ind,min(ind+k,n)):
-                num = max(num,arr[i])
-                s = max(s,(i-ind+1)*(num)+dp(i+1))
-            memo[ind] = s
-            return s
-        ans = dp(0)
+            num = 0
+            for j in range(i,min(i+k,n)):
+                num = max(num,arr[j])
+                s = max(s,(j-i+1)*(num) + dp[j+1])
+            dp[i] = s
+        return dp[0]
+
         return ans
