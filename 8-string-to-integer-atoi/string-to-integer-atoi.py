0d3087d2614fe83c097d7f26e49d96b1e905c264
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-24 17:59:47 -0500

    Time: 7 ms (5.88%) | Memory: 18 MB (29.56%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,41 +1,29 @@
 class Solution:
-    def myAtoi(self, input: str) -> int:
-        sign = 1
-        result = 0
-        index = 0
-        n = len(input)
-
-        INT_MAX = pow(2, 31) - 1
-        INT_MIN = -pow(2, 31)
-
-        # Discard all spaces from the beginning of the input string.
-        while index < n and input[index] == " ":
-            index += 1
-
-        # sign = +1, if it's positive number, otherwise sign = -1.
-        if index < n and input[index] == "+":
-            sign = 1
-            index += 1
-        elif index < n and input[index] == "-":
-            sign = -1
-            index += 1
-
-        # Traverse next digits of input and stop if it is not a digit.
-        # End of string is also non-digit character.
-        while index < n and input[index].isdigit():
-            digit = int(input[index])
-
-            # Check overflow and underflow conditions.
-            if (result > INT_MAX // 10) or (
-                result == INT_MAX // 10 and digit > INT_MAX % 10
-            ):
-                # If integer overflowed return 2^31-1, otherwise if underflowed return -2^31.
-                return INT_MAX if sign == 1 else INT_MIN
-
-            # Append current digit to the result.
-            result = 10 * result + digit
-            index += 1
-
-        # We have formed a valid number without any overflow/underflow.
-        # Return it after multiplying it with its sign.
-        return sign * result
+    def myAtoi(self, s: str) -> int:
+        n = len(s)
+        i = 0
+        sign = "+"
+        ans = 0
+        stack = []
+        MIN_INT = -(2**31)
+        MAX_INT = (2**31 - 1)
+        while i<n and s[i] == " ":
+            i+=1
+        if i<n and s[i] in "+-":
+            sign = s[i]
+            i+=1
+        while i<n and s[i]>='0' and s[i]<='9':
+            stack.append(int(s[i]))
+            i+=1
+        sz = len(stack)
+        j = sz
+        while stack:
+            num = stack.pop()
+            ans = ans+num*(10**(j-sz))
+            sz-=1
+        ans = ans if sign == "+" else -1*ans
+        if ans < MIN_INT:
+            ans = MIN_INT
+        if ans>MAX_INT:
+            ans = MAX_INT
+        return ans
