94cfa440254b6728cd472b6f3d8f85f12a2838a9
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-02-26 08:57:03 -0500

    Time: 123 ms (58.24%) | Memory: 19.3 MB (8.29%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,29 +1,26 @@
+from collections import Counter
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        count_map = Counter(tasks)
-        lastIndex_map = {}
-        lastIndex_map = defaultdict(lambda:-n-1,lastIndex_map)
-        ans = 0
-        string = ""
-        while len(count_map.keys())>0:
-            keys = count_map.keys()
-            keys = sorted(keys, key = lambda x:count_map[x],reverse = True)
-            isUpdate = False
-            for key in keys:
-                if ans-lastIndex_map[key] >n:
-                    lastIndex_map[key] = ans
-                    count_map[key]-=1
-                    string+=key
-                    ans+=1
-                    if count_map[key] == 0:
-                        del count_map[key]
-                        del lastIndex_map[key]
-                    isUpdate = True
-                    break
-            if not isUpdate:
-                string+=" "
-                ans+=1
-        print(string)
-        return len(string)
-            
-        
+        counter = Counter(tasks)
+        pq = [(-v,ord(k)-ord("A")) for k,v in counter.items()]
+        heapq.heapify(pq)
+        count = 0
+        res = []
+        while pq:
+            cycles = n+1
+            temp_arr = {}
+            while cycles>0 and pq:
+                top,ind = heapq.heappop(pq)
+                top = -top-1
+                if top:
+                    temp_arr[ind] = top
+                count+=1
+                res.append(ind)
+                cycles-=1
+            if cycles>0 and temp_arr:
+                count+=cycles
+                res.append(-1)
+            for i,f in temp_arr.items():
+                heapq.heappush(pq,(-f,i))
+        print(res)
+        return count
