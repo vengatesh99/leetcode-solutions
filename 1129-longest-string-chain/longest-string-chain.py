fe13d89069b08fe6305c0f3d7b536e9b4803a694
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-03-19 14:38:28 -0400

    Time: 1022 ms (12.16%) | Memory: 18.2 MB (63.84%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.py
+++ 1129-longest-string-chain/longest-string-chain.py
@@ -1,17 +1,27 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
+        words = sorted(words, key = lambda x: len(x))
         dp = [1 for _ in words]
-        words.sort(key = lambda x: len(x))
-        def isChain(w1,w2):
+        def isPredecessor(word_i,word_j):
+            if len(word_i)-len(word_j)>1 or len(word_i)==len(word_j):
+                return False
             c = 0
-            for k in range(len(w2)):
-                if w2[:k]+w2[k+1:] == w1:
+            k = 0
+            p = 0
+            while k<len(word_j) and p<len(word_i):
+                if word_j[k]!=word_i[p]:
                     c+=1
-                    break
-            return c!=0
-            
+                else:
+                    k+=1
+                p+=1
+            return c<3 and k == len(word_j)
         for i in range(1,len(words)):
+            m = 1
             for j in range(i-1,-1,-1):
-                if len(words[i])-len(words[j]) == 1 and isChain(words[j],words[i]):
-                    dp[i] = max(dp[i],dp[j]+1)
+                if isPredecessor(words[i],words[j]):
+                    m = max(m,1+dp[j])
+                    print(words[j],words[i])
+            dp[i] = m
+        print(words)
+        print(dp)
         return max(dp)
