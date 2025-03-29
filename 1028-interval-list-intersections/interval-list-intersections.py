821b64873fa5939b1c916384001828736dc7668b
Vengatesh Deen Dayal <31369176+vengatesh99@users.noreply.github.com>
2025-03-29 12:23:41 -0400

    Time: 15 ms (5.18%) | Memory: 18.9 MB (23.63%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.py
+++ 1028-interval-list-intersections/interval-list-intersections.py
@@ -1,15 +1,25 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        n,m = len(firstList),len(secondList)
-        i = j = 0
-        intersection = []
-        while i<n and j<m:
-            lo = max(firstList[i][0],secondList[j][0])
-            hi = min(firstList[i][1],secondList[j][1])
-            if lo<=hi:
-                intersection.append([lo,hi])
-            if firstList[i][1]<secondList[j][1]:
-                i+=1
-            else:
+        i,j = 0,0
+        while i<len(firstList) and j<len(secondList):
+            if firstList[i][0]>=secondList[j][0]:
+                firstList.insert(i,secondList[j])
                 j+=1
-        return intersection
+            i+=1
+        while j<len(secondList):
+            firstList.append(secondList[j])
+            j+=1
+        print(firstList)
+        i = 1
+        result = []
+        while i<len(firstList):
+            j = i-1
+            if i<len(firstList) and firstList[j][1]>=firstList[i][0]:
+                while i<len(firstList) and firstList[j][1]>=firstList[i][0]:
+                    result.append([firstList[i][0],min(firstList[i][1],firstList[j][1])])
+                    i+=1
+            else:
+                i+=1
+        return result
+
+            
